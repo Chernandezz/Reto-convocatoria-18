00afcc2081d48aeb079161cef8dc1da15ab488f4
Crisitian Felipe Hernandez Zuluaga <chernandezz@unal.edu.co>
2022-03-04 00:07:48 -0500

    Se Agregaron los archivos de la base de datos

---
+++ BD/parqueadero.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-03-2022 a las 06:00:28
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `parqueadero`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro`
+--
+
+CREATE TABLE `registro` (
+  `placa` varchar(20) NOT NULL,
+  `tipoCliente` varchar(45) DEFAULT NULL,
+  `tiempoAcumulado` int(10) DEFAULT NULL,
+  `ingreso` varchar(20) DEFAULT NULL,
+  `salida` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `registro`
+--
+
+INSERT INTO `registro` (`placa`, `tipoCliente`, `tiempoAcumulado`, `ingreso`, `salida`) VALUES
+('SDP094', 'Oficial', 0, '11:26', ' '),
+('WRO32E', 'Invitado', 0, '10:27', ' ');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `registro`
+--
+ALTER TABLE `registro`
+  ADD PRIMARY KEY (`placa`),
+  ADD UNIQUE KEY `placa_UNIQUE` (`placa`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
