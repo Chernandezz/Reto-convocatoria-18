c2ead3c63380228cb380c9b185b84d4c775593b7
Cristian Hernandez <chernandezz@unal.edu.co>
2023-01-04 21:05:12 -0500

    Conexiones en la BD

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,13 +1,22 @@
  <?php
 
-function conectarDB(): mysqli
-{
-    $db = new mysqli('localhost', 'root', '', 'parqueadero');
+    function conectarDB(): mysqli
+    {
+        $db = new mysqli('us-cdbr-east-06.cleardb.net', 'bc6f5ee1a3d859', 'cf1daabf', 'heroku_2539db26cc8b03f');
 
-    if (!$db) {
-        echo "Error no se pudo conectar";
-        exit;
+        if (!$db) {
+            echo "Error no se pudo conectar";
+            exit;
+        }
+
+        return $db;
     }
 
-    return $db;
-}
+/*
+mysql://bc6f5ee1a3d859:cf1daabf@us-cdbr-east-06.cleardb.net/heroku_2539db26cc8b03f?reconnect=true
+Username: bc6f5ee1a3d859
+Password: cf1daabf
+Server: us-cdbr-east-06.cleardb.net
+Port: 3306
+Database Name: heroku_2539db26cc8b03f
+*/
